d1270a010c08ed9a79d4357e8e14641ebf1703d8
kidus mulugeta <kmulugeta036@gmail.com>
2026-04-01 18:53:53 +0300

    the match model added

--- search/models.py
+++ search/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
-# Create your models here.
+
+from django.db import models
+from lost_items.models import LostItem
+from found_items.models import FoundItem
+
+class Match(models.Model):
+    lost_item = models.ForeignKey(LostItem, on_delete=models.CASCADE)
+    found_item = models.ForeignKey(FoundItem, on_delete=models.CASCADE)
+
+    similarity_score = models.FloatField()  # how close they match
+    is_confirmed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Match: {self.lost_item.title} ↔ {self.found_item.title}"
