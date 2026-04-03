ce1778637caa712372d636222f689781d7c3f01b
kidus mulugeta <kmulugeta036@gmail.com>
2026-04-03 23:11:58 +0300

    mapped to the configuration url

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/lost/', include('lost_items.urls')),  # Lost Items API
+    path('api/search/', include('search.urls')),  # Search API
 ]
 
 if settings.DEBUG:
