6c76a6ead6a998f37058ded6db535c63d6102547
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2026-04-04 16:21:16 +0300

    update founditem model

--- found_items/models.py
+++ found_items/models.py
@@ -9,3 +9,6 @@ class FoundItem(models.Model):
     status = models.CharField(max_length=20, default='found')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
