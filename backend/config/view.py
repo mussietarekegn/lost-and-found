9aa8291b376de0f2445bb96f3decd2bfc6595b5a
Your Name <you@example.com>
2026-04-06 02:34:41 +0300

    ''

--- backend/config/view.py
+++ backend/config/view.py
@@ -5,22 +5,6 @@ def home(request):
     api_info = """
 Welcome to the Lost & Found App API!
 
-Available API endpoints:
-
-Lost Items:
-- GET all: api/list/
-- GET one: api/<id>/
-- POST create: api/create/
-- PUT update: api/<id>/update/
-- DELETE delete: api/<id>/delete/
-
-Found Items:
-- GET all: api/list/
-- GET one: api/<id>/
-- POST create: api/create/
-- PUT update: api/<id>/update/
-- DELETE delete: api/<id>/delete/
-
-Use a tool like Postman to test these endpoints.
+use postman api for testing tool and test the endpoints:
 """
     return HttpResponse(api_info, content_type="text/plain")
